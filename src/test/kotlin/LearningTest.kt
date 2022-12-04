4caefd95670405ce8e68fc56db2f07a34d015f5b
정현준 <jeongdalma@gmail.com>
2022-12-04 15:15:26 +0900

    copy() 테스트 코드 추가

--- src/test/kotlin/LearningTest.kt
+++ src/test/kotlin/LearningTest.kt
@@ -11,15 +11,25 @@ class LearningTest {
 
     @Test
     fun equals() {
-        var employee1 = Employee("First" , "First");
-        var employee2 = Employee("First" , "First");
+        var employee1 = Employee("First" , "First")
+        var employee2 = Employee("First" , "First")
 
-        assertEquals(employee1.hashCode(), employee2.hashCode());
-        assertEquals(employee1, employee2);
+        assertEquals(employee1.hashCode(), employee2.hashCode())
+        assertEquals(employee1, employee2)
 
-        employee2.name = "second";
+        employee2.name = "second"
 
-        assertNotEquals(employee1.hashCode() , employee2.hashCode());
-        assertNotEquals(employee1, employee2);
+        assertNotEquals(employee1.hashCode() , employee2.hashCode())
+        assertNotEquals(employee1, employee2)
+    }
+
+    @Test
+    fun copy() {
+        var employee1 = Employee("Origin" , "Origin")
+        var employee2 = employee1.copy(name = "Updated");
+
+        assertNotEquals(employee1.hashCode(), employee2.hashCode())
+        assertNotEquals(employee1.name, employee2.name)
+        assertEquals(employee1.email, employee2.email)
     }
 }

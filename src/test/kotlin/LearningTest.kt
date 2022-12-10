6b1a40268dff434ac92e33e74ca91913442e11bd
정현준 <jeongdalma@gmail.com>
2022-12-10 19:46:01 +0900

    mutableList 테스트 코드

--- src/test/kotlin/LearningTest.kt
+++ src/test/kotlin/LearningTest.kt
@@ -1,6 +1,6 @@
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test
-import java.util.EmptyStackException
 import kotlin.test.assertNotEquals
 
 class LearningTest {
@@ -49,5 +49,21 @@ class LearningTest {
         var employee3 = Employee("Origin", "Origin")
 
         assertNotEquals(System.identityHashCode(employee1), System.identityHashCode(employee3))
+    }
+
+    @Test
+    fun mutableList() {
+        val strings = arrayOf("first" , "second", "third")
+        val firstList: List<String> = mutableListOf(*strings)
+
+        /**
+         * 코틀린의 List와 Collection 인터페이스는 상태를 변경하는 메소드가 정의되어 있지 않다.
+         * 가변으로 사용하고 싶다면 MutableList 타입으로 사용하여야 한다.
+         */
+
+        val secondList: MutableList<String> = mutableListOf(*strings)
+        secondList.removeAt(0)
+
+        assertEquals(secondList.size, strings.size - 1)
     }
 }

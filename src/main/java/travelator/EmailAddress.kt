8cdfec4df37aaafccefe62451148298aacc8dc25
정현준 <jeongdalma@gmail.com>
2022-12-25 14:33:38 +0900

    9.1 인라이닝

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -14,13 +14,12 @@ data class EmailAddress(
     companion object {
         @JvmStatic
         fun parse(value: String): EmailAddress {
-            val atIndex = value.lastIndexOf('@')
-            require(!(atIndex < 1 || atIndex == value.length - 1)) {
+            require(!(value.lastIndexOf('@') < 1 || value.lastIndexOf('@') == value.length - 1)) {
                 "EmailAddress must be two parts separated by @"
             }
             return EmailAddress(
-                value.substring(0, atIndex),
-                value.substring(atIndex + 1)
+                value.substring(0, value.lastIndexOf('@')),
+                value.substring(value.lastIndexOf('@') + 1)
             )
         }
     }

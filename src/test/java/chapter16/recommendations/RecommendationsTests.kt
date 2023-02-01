42c8c7881bbce3ddce328b5faca395093ba706b5
jeongdalma <jeongdalma@gmail.com>
2023-02-01 12:41:34 +0900

    17장 목에서 맵으로 테스트 코드 수정

--- src/test/java/chapter16/recommendations/RecommendationsTests.kt
+++ src/test/java/chapter16/recommendations/RecommendationsTests.kt
@@ -11,136 +11,136 @@ import org.junit.jupiter.api.Test
 import java.util.Set
 
 class RecommendationsTests {
-    private val featuredDestinations = mutableMapOf<Location, List<FeaturedDestination>>().withDefault { emptyList() }
-    private val distanceInMetersBetween = mutableMapOf<Pair<Location, Location>, Int>().withDefault { -1 }
+    companion object {
+        val distances = mapOf(
+            (paris to eiffelTower.location) to 5000,
+            (paris to louvre.location) to 1000,
+            (alton to flowerFarm.location) to 5300,
+            (alton to watercressLine.location) to 320,
+            (froyle to flowerFarm.location) to 0,
+            (froyle to watercressLine.location) to 6300
+        )
+    }
 
-    private val recommendations = Recommendations(
-        featuredDestinations::getValue,
-        distanceInMetersBetween::getValue
-    )
-
-    private val paris = location("Paris")
-    private val louvre = featured("Louvre", "Rue de Rivoli")
-    private val eiffelTower = featured("Eiffel Tower", "Champ de Mars")
-    private val alton = location("Alton")
-    private val froyle = location("Froyle")
-    private val watercressLine = featured("Watercress Line", "Alton Station")
-    private val flowerFarm = featured("West End Flower Farm", froyle)
 
     @Test
     fun returns_no_recommendations_when_no_locations() {
-        assertEquals(
-            emptyList<Any>(),
-            recommendations.recommendationsFor(emptySet())
+        check(
+            featuredDestinations = emptyMap(),
+            distances = distances,
+            recommendations = emptySet(),
+            shouldReturn = emptyList()
         )
     }
+
 
     @Test
     fun returns_no_recommendations_when_no_featured() {
-        givenFeaturedDestinationsFor(paris, emptyList())
-        assertEquals(
-            emptyList<Any>(),
-            recommendations.recommendationsFor(Set.of(paris))
+        check(
+            featuredDestinations = emptyMap(),
+            distances = distances,
+            recommendations = setOf(paris),
+            shouldReturn = emptyList()
         )
     }
+
 
     @Test
     fun returns_recommendations_for_single_location() {
-        givenFeaturedDestinationsFor(
-            paris,
-            java.util.List.of(
-                eiffelTower,
-                louvre
-            )
-        )
-        givenADistanceBetween(paris, eiffelTower, 5000)
-        givenADistanceBetween(paris, louvre, 1000)
-        assertEquals(
-            java.util.List.of(
+        check(
+            featuredDestinations = mapOf(
+                paris to listOf(eiffelTower, louvre),
+            ),
+            distances = distances,
+            recommendations = setOf(paris),
+            shouldReturn = listOf(
                 FeaturedDestinationSuggestion(paris, louvre, 1000),
                 FeaturedDestinationSuggestion(paris, eiffelTower, 5000)
-            ),
-            recommendations.recommendationsFor(Set.of(paris))
+            )
         )
     }
 
+
     @Test
     fun returns_recommendations_for_multi_location() {
-        givenFeaturedDestinationsFor(
-            paris,
-            java.util.List.of(
-                eiffelTower,
-                louvre
-            )
-        )
-        givenADistanceBetween(paris, eiffelTower, 5000)
-        givenADistanceBetween(paris, louvre, 1000)
-        givenFeaturedDestinationsFor(
-            alton,
-            java.util.List.of(
-                flowerFarm,
-                watercressLine
-            )
-        )
-        givenADistanceBetween(alton, flowerFarm, 5300)
-        givenADistanceBetween(alton, watercressLine, 320)
-        assertEquals(
-            java.util.List.of(
+        check(
+            featuredDestinations = mapOf(
+                paris to listOf(eiffelTower, louvre),
+                alton to listOf(flowerFarm, watercressLine),
+            ),
+            distances = distances,
+            recommendations = setOf(paris, alton),
+            shouldReturn = listOf(
                 FeaturedDestinationSuggestion(alton, watercressLine, 320),
                 FeaturedDestinationSuggestion(paris, louvre, 1000),
                 FeaturedDestinationSuggestion(paris, eiffelTower, 5000),
                 FeaturedDestinationSuggestion(alton, flowerFarm, 5300)
-            ),
-            recommendations.recommendationsFor(Set.of(paris, alton))
+            )
         )
     }
 
     @Test
     fun deduplicates_using_smallest_distance() {
-        givenFeaturedDestinationsFor(alton, listOf(flowerFarm, watercressLine))
-        givenADistanceBetween(alton, flowerFarm, 5300)
-        givenADistanceBetween(alton, watercressLine, 320)
-
-        givenFeaturedDestinationsFor(froyle, listOf(flowerFarm, watercressLine))
-        givenADistanceBetween(froyle, flowerFarm, 0)
-        givenADistanceBetween(froyle, watercressLine, 6300)
-
-        assertEquals(
-            listOf(
+        check(
+            featuredDestinations = mapOf(
+                alton to listOf(flowerFarm, watercressLine),
+                froyle to listOf(flowerFarm, watercressLine)
+            ),
+            distances = distances,
+            recommendations = setOf(alton, froyle),
+            shouldReturn = listOf(
                 FeaturedDestinationSuggestion(froyle, flowerFarm, 0),
                 FeaturedDestinationSuggestion(alton, watercressLine, 320)
-            ),
-            recommendations.recommendationsFor(setOf(alton, froyle))
+            )
         )
-    }
-
-    private fun location(name: String): Location {
-        return Location(Id.of(name), name, name)
-    }
-
-    private fun featured(name: String, locationName: String): FeaturedDestination {
-        return featured(name, location(locationName))
-    }
-
-    private fun featured(name: String, location: Location): FeaturedDestination {
-        return FeaturedDestination(name, location)
-    }
-
-    private fun givenFeaturedDestinationsFor(
-        location: Location,
-        destinations: List<FeaturedDestination>
-    ) {
-        featuredDestinations[location] = destinations.toList()
-    }
-
-    private fun givenADistanceBetween(
-        location: Location,
-        destination: FeaturedDestination,
-        distanceInMeters: Int
-    ) {
-        distanceInMetersBetween[location to destination.location] =
-            distanceInMeters
     }
 }
 
-private fun <K1, K2, V> Map<Pair<K1, K2>, V>.getValue(k1: K1, k2: K2) = getValue(k1 to k2)
+private fun subjectFor(
+    featuredDestinations: Map<Location, List<FeaturedDestination>>,
+    distances: Map<Pair<Location, Location>, Int>
+): Recommendations {
+    val destinationsLookup = featuredDestinations.withDefault { emptyList() }
+    val distanceLookup = distances.withDefault { -1 }
+    return Recommendations(destinationsLookup::getValue, distanceLookup::getValue)
+}
+
+private fun check(
+    featuredDestinations: Map<Location, List<FeaturedDestination>>,
+    distances: Map<Pair<Location, Location>, Int>,
+    recommendations: kotlin.collections.Set<Location>,
+    shouldReturn: List<FeaturedDestinationSuggestion>
+) {
+    assertEquals(
+        shouldReturn,
+        resultFor(featuredDestinations, distances, recommendations)
+    )
+}
+
+private fun resultFor(
+    featuredDestinations: Map<Location, List<FeaturedDestination>>,
+    distances: Map<Pair<Location, Location>, Int>,
+    locations: kotlin.collections.Set<Location>
+): List<FeaturedDestinationSuggestion> {
+    val subject = subjectFor(featuredDestinations, distances)
+    return subject.recommendationsFor(locations)
+}
+
+private fun <K1, K2, V> Map<Pair<K1, K2>, V>.getValue(k1: K1, k2: K2) =
+    getValue(k1 to k2)
+
+private val paris = location("Paris")
+private val louvre = featured("Louvre", "Rue de Rivoli")
+private val eiffelTower = featured("Eiffel Tower", "Champ de Mars")
+private val alton = location("Alton")
+private val froyle = location("Froyle")
+private val watercressLine = featured("Watercress Line", "Alton Station")
+private val flowerFarm = featured("West End Flower Farm", froyle)
+
+private fun location(name: String) = Location(Id.of(name), name, name)
+
+private fun featured(name: String, locationName: String) =
+    featured(name, location(locationName))
+
+private fun featured(name: String, location: Location) =
+    FeaturedDestination(name, location)

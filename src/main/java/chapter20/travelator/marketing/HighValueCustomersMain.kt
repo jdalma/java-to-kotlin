43de210f5f4cab81402a816166d329bd7fca0fdf
jeongdalma <jeongdalma@gmail.com>
2023-02-20 12:17:07 +0900

    인라인 함수 적용

--- src/main/java/chapter20/travelator/marketing/HighValueCustomersMain.kt
+++ src/main/java/chapter20/travelator/marketing/HighValueCustomersMain.kt
@@ -1,8 +1,9 @@
 package chapter20.travelator.marketing
 
+import java.io.Closeable
+import java.io.OutputStreamWriter
 import java.io.Reader
 import java.io.Writer
-import kotlin.system.exitProcess
 
 fun main() {
     System.`in`.reader().use { reader ->
@@ -16,29 +17,69 @@ fun main() {
 //            writer.appendLines(
 //                generate(reader.readLines().asSequence().constrainOnce())
 //            )
-            val errorLines = mutableListOf<ParseFailure>()
-            val reportLines = reader
-                .asLineSequence()
-                .toHighValueCustomerReport {
-                    errorLines += it
+//            [3]
+//            val errorLines = mutableListOf<ParseFailure>()
+//            val reportLines = reader
+//                .asLineSequence()
+//                .toHighValueCustomerReport {
+//                    errorLines += it
+//                }
+//            if (errorLines.isNotEmpty()) {
+//                System.err.writer().use {error ->
+//                    error.appendLine("Lines with errors")
+//                    errorLines.asSequence().map { parseFailure ->
+//                        "${parseFailure::class.simpleName} in ${parseFailure.line}"
+//                    }.writeTo(error)
+//                }
+//                exitProcess(-1)
+//            } else {
+//                reportLines.writeTo(writer)
+//            }
+            val statusCode = using(
+                System.`in`.reader(),
+                System.out.writer(),
+                System.err.writer()
+            ) { reader, writer, error ->
+                val errorLines = mutableListOf<ParseFailure>()
+                val reportLines = reader
+                    .asLineSequence()
+                    .toHighValueCustomerReport {
+                        errorLines += it
+                    }
+                if (errorLines.isEmpty()) {
+                    reportLines.writeTo(writer)
+                    0
+                } else {
+                    errorLines.writeTo(error)
+                    -1
                 }
-            if (errorLines.isNotEmpty()) {
-                System.err.writer().use {error ->
-                    error.appendLine("Lines with errors")
-                    errorLines.asSequence().map { parseFailure ->
-                        "${parseFailure::class.simpleName} in ${parseFailure.line}"
-                    }.writeTo(error)
-                }
-                exitProcess(-1)
-            } else {
-                reportLines.writeTo(writer)
             }
         }
     }
 }
 
+inline fun <A : Closeable, B : Closeable, C : Closeable, R> using(
+    a: A,
+    b: B,
+    c: C,
+    block: (A,B,C) -> R
+) : R =
+    a.use {
+        b.use {
+            c.use {
+                block(a,b,c)
+            }
+        }
+    }
+
 fun Reader.asLineSequence() = buffered().lineSequence()
 
+fun List<ParseFailure>.writeTo(error: OutputStreamWriter) {
+    error.appendLine("Lines with Errors")
+    asSequence().map { parseFailure ->
+        "${parseFailure::class.simpleName} in ${parseFailure.line}"
+    }.writeTo(error)
+}
 fun Sequence<CharSequence>.writeTo(writer: Writer) {
     writer.appendLines(this)
 }

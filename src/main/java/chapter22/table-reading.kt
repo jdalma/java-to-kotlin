3901499c9fe56a2bc39193598acb3241c7bc7ad3
jeongdalma <jeongdalma@gmail.com>
2023-02-21 13:02:16 +0900

    22장 구분자 추상화

--- src/main/java/chapter22/table-reading.kt
+++ src/main/java/chapter22/table-reading.kt
@@ -1,37 +1,63 @@
 package chapter22
 
-import kotlin.reflect.KFunction
 
-
-fun readTableWithHeader(lines: List<String>): List<Map<String, String>> =
+fun readTableWithHeader(
+    lines: List<String>,
+    splitter: (String) -> List<String> = splitOnComma
+): List<Map<String, String>> =
     when {
         lines.isEmpty() -> emptyList()
         else -> readTable(
             lines.drop(1),
-            headerProviderFrom(lines.first())
+            headerProviderFrom(lines.first(), splitter),
+            splitter
         )
     }
 
-fun headerProviderFrom(header: String): (Int) -> String {
-    val headers = header.splitFields(",")
+fun headerProviderFrom(
+    header: String,
+    splitter: (String) -> List<String>
+): (Int) -> String {
+    val headers = splitter(header)
     return {index -> headers[index]}
 }
 
 fun readTable(
     lines: List<String>,
-    headerProvider: (Int) -> String = Int::toString
-): List<Map<String, String>> {
-    return lines.map { parseLine(it, headerProvider) }
-}
+    headerProvider: (Int) -> String = Int::toString,
+    splitter: (String) -> List<String> = splitOnComma
+): List<Map<String, String>> =
+    lines.map {
+//        [1]
+//        parseLine(it, headerProvider) { line ->
+//            line.splitFields(",")
+//        }
+//        [2]
+//        val splitOnComma: (String) -> List<String> = { line ->
+//            line.splitFields(",")
+//        }
+//        parseLine(it, headerProvider, splitOnComma)
+        parseLine(it, headerProvider, splitter)
+    }
 
 private fun parseLine(
     line: String,
-    headerProvider: (Int) -> String
+    headerProvider: (Int) -> String,
+    splitter: (String) -> List<String>
 ): Map<String, String> {
-    val values = line.splitFields(",")
+    val values = splitter(line)
     val keys = values.indices.map(headerProvider)
     return keys.zip(values).toMap()
 }
 
 private fun String.splitFields(separators: String): List<String> =
     if (this.isEmpty()) emptyList() else this.split(separators)
+
+fun splitOn(
+    separators: String
+): (String) -> List<String> = { line ->
+    line.splitFields(separators)
+}
+
+val splitOnComma: (String) -> List<String> = splitOn(",")
+val splitOnTab: (String) -> List<String> = splitOn("\t")
